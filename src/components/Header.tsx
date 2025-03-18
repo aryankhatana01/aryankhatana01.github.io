311ebe7024c7065d3d6a88e16b8cb1a14e9f16ca
aryankhatana01 <aryankhatana01@gmail.com>
2025-03-19 00:44:01 +0530

    added light theme toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { ThemeToggle } from "./theme-toggle";
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -22,40 +23,43 @@ const Header = () => {
     >
       <div className="max-w-5xl mx-auto px-4 flex justify-between items-center">
         <div className="text-lg font-medium">Aryan Khatana</div>
-        <nav className="hidden md:block">
-          <ul className="flex space-x-1">
-            <li>
-              <a href="#about" className="nav-link">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#experience" className="nav-link">
-                Experience
-              </a>
-            </li>
-            <li>
-              <a href="#projects" className="nav-link">
-                Projects
-              </a>
-            </li>
-            <li>
-              <a href="#skills" className="nav-link">
-                Skills
-              </a>
-            </li>
-            <li>
-              <a href="#achievements" className="nav-link">
-                Achievements
-              </a>
-            </li>
-            {/* <li>
-              <a href="#contact" className="nav-link">
-                Contact
-              </a>
-            </li> */}
-          </ul>
-        </nav>
+        <div className="flex items-center gap-4">
+          <nav className="hidden md:block">
+            <ul className="flex space-x-1">
+              <li>
+                <a href="#about" className="nav-link">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#experience" className="nav-link">
+                  Experience
+                </a>
+              </li>
+              <li>
+                <a href="#projects" className="nav-link">
+                  Projects
+                </a>
+              </li>
+              <li>
+                <a href="#skills" className="nav-link">
+                  Skills
+                </a>
+              </li>
+              <li>
+                <a href="#achievements" className="nav-link">
+                  Achievements
+                </a>
+              </li>
+              {/* <li>
+                <a href="#contact" className="nav-link">
+                  Contact
+                </a>
+              </li> */}
+            </ul>
+          </nav>
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   );
